370ade270f1ca45d7626713e9589752eb9ab552f
Zhiyong Liu <zyongliu@thoughtworks.com>
2016-11-11 01:14:03 +0800

    T1

--- src/Man.java
+++ src/Man.java
@@ -2,4 +2,17 @@
  * Created by zyongliu on 11/11/16.
  */
 public class Man {
+    private int position;
+
+    public Man() {
+        this.position = 0;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void roll(int step) {
+        position += step;
+    }
 }
--- test/ManTest.java
+++ test/ManTest.java
@@ -1,5 +1,20 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by zyongliu on 11/11/16.
  */
 public class ManTest {
+
+    public static final int STEP = 1;
+
+    @Test
+    public void should_change_position_when_man_roll() throws Exception {
+        Man man = new Man();
+        int position = man.getPosition();
+        man.roll(STEP);
+        assertThat(man.getPosition(), is(position + STEP));
+    }
 }
